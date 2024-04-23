bfe1ecc8e66b4d2392d549fb8e193373e2d09c2f
ezuker <benoit.carolle.pro@gmail.com>
2024-04-23 16:13:59 +0200

    i need to solved diamond trap

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 14:52:35 by bcarolle          #+#    #+#             */
-/*   Updated: 2024/04/23 14:06:46 by bcarolle         ###   ########.fr       */
+/*   Updated: 2024/04/23 14:40:22 by bcarolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int main( void ) {
 	ClapTrap d = b;
 	ClapTrap e(d);
 
+	std::cout << std::endl;
 	b.attack("copain2");
 	c.takeDamage(b.getAttack());
 	b.beRepaired(10);
 	std::cout << d.getName() << std::endl;
 	std::cout << d.getHealth() << std::endl;
 	e.attack("copain");
+	std::cout << std::endl;
 	return (0);
 }
--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:48:56 by bcarolle          #+#    #+#             */
-/*   Updated: 2024/04/23 14:26:01 by bcarolle         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:06:31 by bcarolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), Frag
 {
 	std::cout << "Constructor called of DiamondTrap" << std::endl;
 	this->_name = name;
-	this->_hit_point = FragTrap::_hit_point; //100
-	this->_attack_point = FragTrap::_attack_point; //30
-	this->_energy_point = ScavTrap::_energy_point; //50
+	// this->_hit_point = FragTrap::_hit_point; //100
+	// this->_attack_point = FragTrap::_attack_point; //30
+	// this->_energy_point = ScavTrap::_energy_point; //50
 }
 
 DiamondTrap::DiamondTrap(DiamondTrap const &copy) : ClapTrap(copy), FragTrap(copy), ScavTrap(copy)
@@ -30,10 +30,10 @@ DiamondTrap::DiamondTrap(DiamondTrap const &copy) : ClapTrap(copy), FragTrap(cop
 
 DiamondTrap	&DiamondTrap::operator=(const DiamondTrap &rhs)
 {
-	this->_name = rhs.DiamondTrap::_name;
-	this->_attack_point = rhs.DiamondTrap::_attack_point;
-	this->_energy_point = rhs.DiamondTrap::_energy_point;
-	this->_hit_point = rhs.DiamondTrap::_hit_point;
+	this->_name = rhs._name;
+	this->_attack_point = rhs._attack_point;
+	this->_energy_point = rhs._energy_point;
+	this->_hit_point = rhs._hit_point;
 	return (*this);
 }
 
